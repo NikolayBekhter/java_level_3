9b2e435be64ec29677f07ae416a9150ddb2cd9ed
BekhterNikolay <n.v.bekhter@mail.ru>
2022-04-30 15:34:23 +0300

    lesson_1

---
+++ src/main/java/lesson_1/Fruit.java
@@ -0,0 +1,7 @@
+package lesson_1;
+
+public abstract class Fruit {
+
+    abstract float getWeight();
+
+}
--- src/main/java/lesson_1/lesson_1.java
+++ src/main/java/lesson_1/lesson_1.java
@@ -3,15 +3,45 @@ package lesson_1;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class lesson_1<T> {
+public class lesson_1 {
     public static void main(String[] args) {
-        Integer array1[] = {1, 2, 3, 4, 5, 6, 7};
-        String array2[] = {"A", "B", "C"};
+        Integer[] array1 = {1, 2, 3, 4, 5, 6, 7};
+        String[] array2 = {"A", "B", "C"};
         regroupElement(0, 6, array1);
         regroupElement(2, 0, array2);
 
         toList(array1);
         toList(array2);
+
+        Box<Orange> or = new Box<Orange>();
+        Box<Orange> or1 = new Box<Orange>();
+        Box<Apple> ap = new Box<Apple>();
+        Box<Apple> ap1 = new Box<Apple>();
+        Box<Apple> ap2 = new Box<Apple>();
+
+        or.addFruit(new Orange());
+        or1.addFruit(new Orange());
+        ap.addFruit(new Apple());
+        ap1.addFruit(new Apple());
+        ap2.addFruit(new Apple());
+
+        or.showBox();
+        or1.showBox();
+        or.pourTo(or1);
+        or.showBox();
+        or1.showBox();
+
+        ap.showBox();
+        ap1.showBox();
+        ap.pourTo(ap1);
+        ap.showBox();
+        ap1.showBox();
+
+        System.out.println(or1.compare(ap1));
+
+        ap2.pourTo(ap1);
+
+        System.out.println(or1.compare(ap1));
     }
 
     public static <T> void regroupElement(int index_1, int index_2, T... array){
